9db48decc4b7555c4cb5f9311ef56a418aa1f387
yatinnn <117296805+yatinnn@users.noreply.github.com>
2026-07-18 23:34:20 +0530

    Add: redundant-connection - submission-1

---
+++ Data Structures & Algorithms/redundant-connection/submission-1.cpp
@@ -0,0 +1,47 @@
+class UnionFind{
+    vector<int>parent;
+    vector<int>size;
+    public:
+            UnionFind(int n){
+                parent.resize(n);
+                size.resize(n);
+                for(int i=0;i<n;i++){
+                    parent[i]=i;
+                    size[i]=1;
+
+                }
+            }
+
+            int find(int x){
+                if(x==parent[x])return x;
+                // return parent[x]=find(parent[x]);
+                return find(parent[x]);
+            }
+
+            bool Union(int u,int v){
+                int pu=find(u);
+                int pv=find(v);
+                if(pu==pv)return false;
+                if(size[pu]<size[pv]){
+                    parent[pu]=pv;
+                    size[pv]+=size[pu];
+                }
+                else{
+                    parent[pv]=pu;
+                    size[pu]+=size[pv];
+                }
+                return true;
+            }
+};
+
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n=edges.size();
+        UnionFind uf(n+1);
+        for(auto it : edges){
+            if(!uf.Union(it[0],it[1]))return {it[0],it[1]};
+        }
+         return {};
+    }
+};
