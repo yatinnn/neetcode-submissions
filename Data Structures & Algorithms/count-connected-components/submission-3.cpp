0bdee9018f18eb18355b9a3f5288722079eb17a2
yatinnn <117296805+yatinnn@users.noreply.github.com>
2026-07-18 23:33:55 +0530

    Add: count-connected-components - submission-3

---
+++ Data Structures & Algorithms/count-connected-components/submission-3.cpp
@@ -0,0 +1,46 @@
+class UnionFind{
+    vector<int>parent;
+    vector<int>size;
+    public:
+            UnionFind(int n){
+                parent.resize(n);
+                size.resize(n);
+                for(int i=0;i<n;i++){
+                    parent[i]=i;
+                    size[i]=1;
+
+                }
+            }
+
+            int find(int x){
+                if(x==parent[x])return x;
+                // return parent[x]=find(parent[x]);
+                return find(parent[x]);
+            }
+
+            bool Union(int u,int v){
+                int pu=find(u);
+                int pv=find(v);
+                if(pu==pv)return false;
+                if(size[pu]<size[pv]){
+                    parent[pu]=pv;
+                    size[pv]+=size[pu];
+                }
+                else{
+                    parent[pv]=pu;
+                    size[pu]+=size[pv];
+                }
+                return true;
+            }
+};
+
+class Solution {
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {
+        UnionFind uf(n);
+        for(auto it : edges){
+            if(uf.Union(it[0],it[1]))n--;
+        }
+         return n;
+    }
+};
