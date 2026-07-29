bcfa2144393ccea9cc83d6054caac8ee3ca1304d
yatinnn <117296805+yatinnn@users.noreply.github.com>
2026-07-29 23:19:38 +0530

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+int hammingWeight(uint32_t n) {
+        int cnt=0;
+        while(n){
+            if(n&1==1)cnt++;
+            n=n>>1;
+        }
+        return cnt;
+}
+    
+    vector<int> countBits(int n) {
+        vector<int>ans;
+        for(int i=0;i<=n;i++){
+            ans.push_back(hammingWeight(i));
+        }    
+        return ans;
+    }
+};
